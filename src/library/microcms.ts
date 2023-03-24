6fc3a89d6be15cdc844e1e6d0e6f2bdcba17e943
CreateWebSiteTeam <yourgitemail@example.com>
2023-03-25 02:01:05 +0900

    feat:prettier導入

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,42 +1,40 @@
-import { createClient, MicroCMSQueries } from "microcms-js-sdk";
+import type { MicroCMSQueries } from 'microcms-js-sdk'
+import { createClient } from 'microcms-js-sdk'
 
 const client = createClient({
-  serviceDomain:import.meta.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.MICROCMS_API_KEY,
-});
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN as string,
+  apiKey: process.env.MICROCMS_API_KEY as string,
+})
 
 export type Blog = {
-  id:string
-  createdAt:string
-  updatedAt:string
-  publishedAt:string 
-  revisedAt:string 
-  title:string 
-  content:string 
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
   eyecatch: {
-    url:string 
-    height:number 
-    width:number
+    url: string
+    height: number
+    width: number
   }
 }
 
 export type BlogResponse = {
-  totalCount:number 
-  offset:number
-  limit:number 
-  contents:Blog[]
+  totalCount: number
+  offset: number
+  limit: number
+  contents: Blog[]
 }
 
-export const getBlogs = async(queries?:MicroCMSQueries) => {
-  return await client.get<BlogResponse>({ endpoint:'blogs', queries})
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+  return await client.get<BlogResponse>({ endpoint: 'blogs', queries })
 }
-export const getBlogDetail = async(
-  contentId:string,
-  queries?:MicroCMSQueries
-) => {
+export const getBlogDetail = async (contentId: string, queries?: MicroCMSQueries) => {
   return await client.getListDetail<Blog>({
-    endpoint:'blogs',
+    endpoint: 'blogs',
     contentId,
-    queries
+    queries,
   })
 }
